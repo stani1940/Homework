93a76fdfb4e6a867e98c32694326a3d46cff3957
stani1940 <stanislav1940@abv.bg>
2015-06-27 22:46:55 +0300

    If Switch
    Php

---
+++ If_and_Switch/zad6.php
@@ -0,0 +1,26 @@
+﻿<?php
+echo "Зад.6
+Да се напише програма, която по въведена цифра от 1 до 9 отпечатва цифрата с думи. Например 2 – two, 6 – six. ";
+$numbers =6;
+echo "<br/>";
+switch ($numbers){
+	case '1': echo "one";
+		break;
+	case '2': echo "two";
+		break;
+	case '3': echo "three";
+		break;
+	case '4': echo "four";
+		break;
+	case '5': echo "five";
+		break;
+	case '6': echo "six";
+		break;
+	case '7': echo "seven";
+		break;
+	case '8': echo "eight";
+		break;
+	case '9': echo "nine";
+		break;
+}
+?>
