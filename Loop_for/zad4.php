225a998d6dfcbe054bb1620c39d577178aaa03b8
stani1940 <stanislav1940@abv.bg>
2015-07-06 00:32:49 +0300

    php
    php

---
+++ Loop_for/zad4.php
@@ -0,0 +1,18 @@
+﻿<?php
+$n=10;
+echo "<table border='1'>";
+for ($i=1; $i <=$n ; $i++) { 
+	echo "<tr>";
+	for ($j=1; $j <=$i ; $j++) { 
+		$h=$i*$j;
+		$b=10-$i;
+		echo "<td>"."$j*$i=$h "."</td>";
+
+	
+	}
+	echo "<td colspan='$b'></td>";
+
+	echo "</tr>";
+}
+echo "</table>";
+?>
